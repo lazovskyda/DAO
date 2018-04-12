edc8fc99c06cff769293c03ea42b26b18820102b
lazovskyda <LazovskyDA@qulix.com>
2018-04-12 17:44:45 +0300

    added valid Junit test

--- src/test/java/com/lazovsky/DAO/PostgresDAOTest.java
+++ src/test/java/com/lazovsky/DAO/PostgresDAOTest.java
@@ -4,11 +4,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -19,9 +22,12 @@ import static org.junit.Assert.*;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(properties = "spring.main.web-application-type=reactive")
 public class PostgresDAOTest {
+
+
+
     @Autowired
-    //@Qualifier("postgresDAO")
     PostgresDAO connection;
+
 
     @Test
     public void insert() {
@@ -31,13 +37,15 @@ public class PostgresDAOTest {
 
         //ApplicationContext context = SpringApplication.run(DaoApplication.class, args);
 
-
         connection.insert(mp3);
 
 
         connection.getByName("InsertMethodTestName");
         assertEquals(mp3.getAuthor(), connection.getByName("InsertMethodTestName").getAuthor());
+        assertEquals(mp3.getName(), connection.getByName("InsertMethodTestName").getName());
         connection.delete(mp3);
 
     }
+
+
 }
